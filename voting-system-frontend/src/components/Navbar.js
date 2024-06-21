6c57b1ed75993cd51ad639a65d1056c96464c041
Noris69 <cheikh.noris69@gmail.com>
2024-06-21 20:39:56 +0100

    Minor changes

--- voting-system-frontend/src/components/Navbar.js
+++ voting-system-frontend/src/components/Navbar.js
@@ -53,10 +53,10 @@ const Navbar = () => {
             <li className="flex">
               {authData && authData.user ? (
                 <>
-                  <span className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Welcome, {authData.user.username}</span>
+                  <span className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"> Welcome, {authData.user.username} </span>
                   <button
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    onClick={handleLogout}
+        className="block py-2 px-3 ml-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+        onClick={handleLogout}
                   >
                     Logout
                   </button>
